2dd5b069bb2f4717171d3285b743a6db4687fae1
mostafasameh911 <144452923+mostafasameh911@users.noreply.github.com>
2023-10-12 09:41:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//let male = ["Mohamed" , "ahmed", "soad" ];
+//let female = ["mona" , "asmaa"];
+//let child = ["atef" , "mostafa"];
+//let total = male.concat(female , child);
+//document.write(total + "<br>");
+//for(let z = 0; z<total.length; z++){document.write(`<h1> your name is ${total[z]}`)};
